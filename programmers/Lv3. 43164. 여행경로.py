2492bdec35ee077b85c4a3615f179063213f7bec
정현진 <hyeonjin507@gmail.com>
2021-02-02 09:44:02 -0800

    Update Lv3. 43164. 여행경로.py

--- programmers/Lv3. 43164. 여행경로.py
+++ programmers/Lv3. 43164. 여행경로.py
@@ -1,4 +1,4 @@
-// https://programmers.co.kr/learn/courses/30/lessons/43164?language=python3
+# https://programmers.co.kr/learn/courses/30/lessons/43164?language=python3
 
 ways = dict()
 answer = []
