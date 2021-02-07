af4b0a1040dd44f3a84414d2d02143e9ca5a7f48
정현진 <hyeonjin507@gmail.com>
2021-02-07 22:33:10 +0900

    Update Lv2. 42860. 조이스틱.cpp
    좀 더 연산 효율적으로 수정

--- programmers/Lv2. 42860. 조이스틱.cpp
+++ programmers/Lv2. 42860. 조이스틱.cpp
@@ -41,9 +41,12 @@ int solution(string name) {
     // 최소일 수 있음
     // (예: "ABABAAAAAAAAAZ"의 경우
     //  끝으로 한번 갔다가 돌아와서(2) 오른쪽으로 3번
-    //  가는게 최소임. 이 경우 답은 8)
+    //  가는게 최소임. 이 경우 답은 8
     
     // 일단 두번째 글자 이후로 A를 찾기
+    int seq_len = 0;
+    int seq_start = 0;
+    int seq_end = 0;
     for (i=1; i<n; ++i) {
         if (name[i] == 'A') {
             i--;
@@ -53,22 +56,34 @@ int solution(string name) {
                     break;
             }
             // i+1 부터 j-1 까지 A가 있음
-            
-            // 이제 i까지 갔다가 돌아와서 j까지 가거나,
-            // j까지 갔다가 돌아와서 i까지 가는 것 중
-            // 더 짧은 것을 찾으면 됨
-            
-            int res;
-            // i까지 갔다가 돌아와서 j까지 가는 것
-            res = i*2 + (n-j);
-            answer = min(answer, res);
-            // j까지 갔다가 돌아와서 i까지 가는 것
-            res = i + (n-j)*2;
-            answer = min(answer, res);
+            // 이게 가장 길게 연속된 A인지 확인하고
+            // 맞다면 기록하기
+            if (j-i+1 > seq_len) {
+                seq_len = j-i+1;
+                seq_start = i;
+                seq_end = j;
+            }
             
             // 이제 j부터 A를 찾아서, 다시 반복함
             i = j;
         }
+    }
+    
+    if (seq_end > 0) {
+        // 가장 A가 긴 경우의 시작점, 끝점
+        i = seq_start;
+        j = seq_end;
+
+        // 이제 i까지 갔다가 돌아와서 j까지 가거나,
+        // j까지 갔다가 돌아와서 i까지 가는 것 중
+        // 더 짧은 것을 찾으면 됨
+        int res;
+        // i까지 갔다가 돌아와서 j까지 가는 것
+        res = i*2 + (n-j);
+        answer = min(answer, res);
+        // j까지 갔다가 돌아와서 i까지 가는 것
+        res = i + (n-j)*2;
+        answer = min(answer, res);
     }
     
     // 이동 횟수의 최소값은 이제 찾았고,
