3573bce79347603b2d23bc4b4da40c8957afc33f
정현진 <hyeonjin507@gmail.com>
2021-02-08 01:44:50 +0900

    Update Lv3. 12938. 최고의 집합.py

--- programmers/Lv3. 12938. 최고의 집합.py
+++ programmers/Lv3. 12938. 최고의 집합.py
@@ -1,13 +1,6 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12938
 
 def solution(n, s):
-    if n > s:
-        return [-1]
-    
-    num = int(s/n)
-    answer = [num for i in range(n)]
-    
-    for i in range(s - n*num):
-        answer[-(1+i)] += 1
-    
-    return answer
+    return [-1] if n > s else [
+        int(s/n) if n-i > s%n else int(s/n) + 1
+        for i in range(n)]
